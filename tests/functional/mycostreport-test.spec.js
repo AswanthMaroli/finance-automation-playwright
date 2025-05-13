b086d5ce8d5e736ee725b8bc080f5d87172537e4
AswanthMaroli <aswanthaswu07@gmail.com>
2025-05-13 14:59:36 +0530

    changes codes mycostreport

--- tests/functional/mycostreport-test.spec.js
+++ tests/functional/mycostreport-test.spec.js
@@ -33,155 +33,155 @@ test.beforeEach( async ({ page }) => {
 
 });
 
-// test("Verify that my cost report page contains all the customer files ", async ({ page }) => {
+test("Verify that my cost report page contains all the customer files ", async ({ page }) => {
 
-//     const homePage = new HomePage(page);
-//     try{
-//       const loginPage = await homePage.clickLoginMenu(); 
-//       const dashboardPage = await loginPage.performLogin(loginData[0].useremail,loginData[0].userpassword);
+    const homePage = new HomePage(page);
+    try{
+      const loginPage = await homePage.clickLoginMenu(); 
+      const dashboardPage = await loginPage.performLogin(loginData[0].useremail,loginData[0].userpassword);
 
-//       await page.waitForURL(`${baseURL}/#/CustomerDashboard`, { timeout: 10000 });
-//       await expect(page).toHaveURL(`${baseURL}/#/CustomerDashboard`, { timeout: 10000 });
-//       const myCostReportPage =await dashboardPage.clickCostReport();
-//       await page.waitForURL(`${baseURL}/#/MyCostReports`, { timeout: 10000 });
-//       await expect(page).toHaveURL(`${baseURL}/#/MyCostReports`);
+      await page.waitForURL(`${baseURL}/#/CustomerDashboard`, { timeout: 10000 });
+      await expect(page).toHaveURL(`${baseURL}/#/CustomerDashboard`, { timeout: 10000 });
+      const myCostReportPage =await dashboardPage.clickCostReport();
+      await page.waitForURL(`${baseURL}/#/MyCostReports`, { timeout: 10000 });
+      await expect(page).toHaveURL(`${baseURL}/#/MyCostReports`);
 
-//       const customerFiles = ["Balance Sheet","Trial Balance","Profit and Loss"," PS&R CSV "," PS&R PDF "," CR Checklist "];
+      const customerFiles = ["Balance Sheet","Trial Balance","Profit and Loss"," PS&R CSV "," PS&R PDF "," CR Checklist "];
 
-//       for(const filename of customerFiles){
+      for(const filename of customerFiles){
 
-//         await myCostReportPage.checkCustomerFiles(filename);
+        await myCostReportPage.checkCustomerFiles(filename);
         
-//       }
+      }
         
-//     }catch(error){
-//       console.error(`Test Failed : ${error.message}`);
-//       throw error;
-//     }
+    }catch(error){
+      console.error(`Test Failed : ${error.message}`);
+      throw error;
+    }
 
-// });
+});
 
-// test("Verify that my cost report page contains all the completed files ", async ({ page }) => {
+test("Verify that my cost report page contains all the completed files ", async ({ page }) => {
 
-//   const homePage = new HomePage(page);
-//   try{
-//     const loginPage = await homePage.clickLoginMenu(); 
-//     const dashboardPage = await loginPage.performLogin(loginData[0].useremail,loginData[0].userpassword);
+  const homePage = new HomePage(page);
+  try{
+    const loginPage = await homePage.clickLoginMenu(); 
+    const dashboardPage = await loginPage.performLogin(loginData[0].useremail,loginData[0].userpassword);
 
-//     await page.waitForURL(`${baseURL}/#/CustomerDashboard`, { timeout: 10000 });
-//     await expect(page).toHaveURL(`${baseURL}/#/CustomerDashboard`, { timeout: 10000 });
-//     const myCostReportPage =await dashboardPage.clickCostReport();
-//     await page.waitForURL(`${baseURL}/#/MyCostReports`, { timeout: 10000 });
-//     await expect(page).toHaveURL(`${baseURL}/#/MyCostReports`);
+    await page.waitForURL(`${baseURL}/#/CustomerDashboard`, { timeout: 10000 });
+    await expect(page).toHaveURL(`${baseURL}/#/CustomerDashboard`, { timeout: 10000 });
+    const myCostReportPage =await dashboardPage.clickCostReport();
+    await page.waitForURL(`${baseURL}/#/MyCostReports`, { timeout: 10000 });
+    await expect(page).toHaveURL(`${baseURL}/#/MyCostReports`);
 
-//     const completedFiles = ["AI File","PI File","SC File","Notes","Additional files and explanations"," CMS Correspondence "];
+    const completedFiles = ["AI File","PI File","SC File","Notes","Additional files and explanations"," CMS Correspondence "];
 
-//     for(const filename of completedFiles){
+    for(const filename of completedFiles){
 
-//       await myCostReportPage.checkCompletedFiles(filename);
+      await myCostReportPage.checkCompletedFiles(filename);
       
-//     }
+    }
       
-//   }catch(error){
-//     console.error(`Test Failed : ${error.message}`);
-//     throw error;
-//   }
+  }catch(error){
+    console.error(`Test Failed : ${error.message}`);
+    throw error;
+  }
 
 
-// });
+});
 
 
 
-// test("Verify user can upload customer files ", async ({ page }) => {
+test("Verify user can upload customer files ", async ({ page }) => {
 
-//   const homePage = new HomePage(page);
-//   try{
-//     const loginPage = await homePage.clickLoginMenu(); 
-//     const dashboardPage = await loginPage.performLogin(loginData[0].useremail,loginData[0].userpassword);
+  const homePage = new HomePage(page);
+  try{
+    const loginPage = await homePage.clickLoginMenu(); 
+    const dashboardPage = await loginPage.performLogin(loginData[0].useremail,loginData[0].userpassword);
 
-//     await page.waitForURL(`${baseURL}/#/CustomerDashboard`, { timeout: 10000 });
-//     await expect(page).toHaveURL(`${baseURL}/#/CustomerDashboard`, { timeout: 10000 });
-//     const myCostReportPage =await dashboardPage.clickCostReport();
-//     await page.waitForURL(`${baseURL}/#/MyCostReports`, { timeout: 10000 });
-//     await expect(page).toHaveURL(`${baseURL}/#/MyCostReports`);
+    await page.waitForURL(`${baseURL}/#/CustomerDashboard`, { timeout: 10000 });
+    await expect(page).toHaveURL(`${baseURL}/#/CustomerDashboard`, { timeout: 10000 });
+    const myCostReportPage =await dashboardPage.clickCostReport();
+    await page.waitForURL(`${baseURL}/#/MyCostReports`, { timeout: 10000 });
+    await expect(page).toHaveURL(`${baseURL}/#/MyCostReports`);
 
-//     const customerFiles = ["Balance Sheet","Trial Balance","Profit and Loss"," PS&R CSV "," PS&R PDF "];
+    const customerFiles = ["Balance Sheet","Trial Balance","Profit and Loss"," PS&R CSV "," PS&R PDF "];
 
-//     for (const element of customerFiles) {
+    for (const element of customerFiles) {
 
-//       await myCostReportPage.uploadCustomerFile(element, filePath);
-//       await page.goBack();
-//   }
+      await myCostReportPage.uploadCustomerFile(element, filePath);
+      await page.goBack();
+  }
       
-//   }catch(error){
-//     console.error(`File upload Failed : ${error.message}`);
-//     throw error;
-//   }
+  }catch(error){
+    console.error(`File upload Failed : ${error.message}`);
+    throw error;
+  }
 
 
-// });
+});
 
 
-// test("Verify that user can remove the uploaded files", async ({ page }) => {
+test("Verify that user can remove the uploaded files", async ({ page }) => {
 
-//   const homePage = new HomePage(page);
-//   try{
-//     const loginPage = await homePage.clickLoginMenu(); 
-//     const dashboardPage = await loginPage.performLogin(loginData[0].useremail,loginData[0].userpassword);
+  const homePage = new HomePage(page);
+  try{
+    const loginPage = await homePage.clickLoginMenu(); 
+    const dashboardPage = await loginPage.performLogin(loginData[0].useremail,loginData[0].userpassword);
 
-//     await page.waitForURL(`${baseURL}/#/CustomerDashboard`, { timeout: 10000 });
-//     await expect(page).toHaveURL(`${baseURL}/#/CustomerDashboard`, { timeout: 10000 });
-//     const myCostReportPage =await dashboardPage.clickCostReport();
-//     await page.waitForURL(`${baseURL}/#/MyCostReports`, { timeout: 10000 });
-//     await expect(page).toHaveURL(`${baseURL}/#/MyCostReports`);
+    await page.waitForURL(`${baseURL}/#/CustomerDashboard`, { timeout: 10000 });
+    await expect(page).toHaveURL(`${baseURL}/#/CustomerDashboard`, { timeout: 10000 });
+    const myCostReportPage =await dashboardPage.clickCostReport();
+    await page.waitForURL(`${baseURL}/#/MyCostReports`, { timeout: 10000 });
+    await expect(page).toHaveURL(`${baseURL}/#/MyCostReports`);
 
-//     const customerFiles = ["Balance Sheet","Trial Balance","Profit and Loss"," PS&R CSV "," PS&R PDF "];
+    const customerFiles = ["Balance Sheet","Trial Balance","Profit and Loss"," PS&R CSV "," PS&R PDF "];
 
-//     for (const element of customerFiles) {
+    for (const element of customerFiles) {
 
-//       await myCostReportPage.removeUploadedFile(element);
-//       await page.goBack();  
-//   }
+      await myCostReportPage.removeUploadedFile(element);
+      await page.goBack();  
+  }
       
-//   }catch(error){
-//     console.error(`File Remove Failed : ${error.message}`);
-//     throw error;
-//   }
+  }catch(error){
+    console.error(`File Remove Failed : ${error.message}`);
+    throw error;
+  }
 
-// });
+});
 
 
-// test("Verify that validation message is visible for the mandatory fields", async ({ page }) => {
+test("Verify that validation message is visible for the mandatory fields", async ({ page }) => {
 
-//   const homePage = new HomePage(page);
-//   try{
-//     const loginPage = await homePage.clickLoginMenu(); 
-//     const dashboardPage = await loginPage.performLogin(loginData[0].useremail,loginData[0].userpassword);
+  const homePage = new HomePage(page);
+  try{
+    const loginPage = await homePage.clickLoginMenu(); 
+    const dashboardPage = await loginPage.performLogin(loginData[0].useremail,loginData[0].userpassword);
 
-//     await page.waitForURL(`${baseURL}/#/CustomerDashboard`, { timeout: 10000 });
-//     await expect(page).toHaveURL(`${baseURL}/#/CustomerDashboard`, { timeout: 10000 });
-//     const myCostReportPage =await dashboardPage.clickCostReport();
-//     await page.waitForURL(`${baseURL}/#/MyCostReports`, { timeout: 10000 });
-//     await expect(page).toHaveURL(`${baseURL}/#/MyCostReports`);
-//     await myCostReportPage.clickCRCheckList();
-//     await page.waitForURL(`${baseURL}/#/CreateCostReport`, { timeout: 10000 });
-//     await expect(page).toHaveURL(`${baseURL}/#/CreateCostReport`);
-//     await myCostReportPage.clickSubmitBtn();
-//     await myCostReportPage.isFieldValidationIsVisible(myCostReportPage.hhaNameValidationSection,"Enter HHA name");
-//     await myCostReportPage.isFieldValidationIsVisible(myCostReportPage.hhaAddressValidationSection,"Enter HHA Address");
-//     await myCostReportPage.isFieldValidationIsVisible(myCostReportPage.hhaMedicareProviderNumberValidationSection,"Please enter Mediare Provider Number");
-//     await myCostReportPage.isFieldValidationIsVisible(myCostReportPage.hhaCityValidationSection,"Select City");
-//     await myCostReportPage.isFieldValidationIsVisible(myCostReportPage.hhaStateValidationSection,"Select State");
-//     await myCostReportPage.isFieldValidationIsVisible(myCostReportPage.hhaZipValidationSection,"Enter Zip code");
-//     await myCostReportPage.isFieldValidationIsVisible(myCostReportPage.hhaDateOfCertifiedValidationSection,"Enter Date certified medicare");
-//     await myCostReportPage.isFieldValidationIsVisible(myCostReportPage.hhaTypeOfOrganizationValidationSection,"Select type of organization");
+    await page.waitForURL(`${baseURL}/#/CustomerDashboard`, { timeout: 10000 });
+    await expect(page).toHaveURL(`${baseURL}/#/CustomerDashboard`, { timeout: 10000 });
+    const myCostReportPage =await dashboardPage.clickCostReport();
+    await page.waitForURL(`${baseURL}/#/MyCostReports`, { timeout: 10000 });
+    await expect(page).toHaveURL(`${baseURL}/#/MyCostReports`);
+    await myCostReportPage.clickCRCheckList();
+    await page.waitForURL(`${baseURL}/#/CreateCostReport`, { timeout: 10000 });
+    await expect(page).toHaveURL(`${baseURL}/#/CreateCostReport`);
+    await myCostReportPage.clickSubmitBtn();
+    await myCostReportPage.isFieldValidationIsVisible(myCostReportPage.hhaNameValidationSection,"Enter HHA name");
+    await myCostReportPage.isFieldValidationIsVisible(myCostReportPage.hhaAddressValidationSection,"Enter HHA Address");
+    await myCostReportPage.isFieldValidationIsVisible(myCostReportPage.hhaMedicareProviderNumberValidationSection,"Please enter Mediare Provider Number");
+    await myCostReportPage.isFieldValidationIsVisible(myCostReportPage.hhaCityValidationSection,"Select City");
+    await myCostReportPage.isFieldValidationIsVisible(myCostReportPage.hhaStateValidationSection,"Select State");
+    await myCostReportPage.isFieldValidationIsVisible(myCostReportPage.hhaZipValidationSection,"Enter Zip code");
+    await myCostReportPage.isFieldValidationIsVisible(myCostReportPage.hhaDateOfCertifiedValidationSection,"Enter Date certified medicare");
+    await myCostReportPage.isFieldValidationIsVisible(myCostReportPage.hhaTypeOfOrganizationValidationSection,"Select type of organization");
       
-//   }catch(error){
-//     console.error(`Validation check test failed : ${error.message}`);
-//     throw error;
-//   }
+  }catch(error){
+    console.error(`Validation check test failed : ${error.message}`);
+    throw error;
+  }
 
-// });
+});
 
 test("Verify that user can submit the CR Checklist form with valid data", async ({ page }) => {
 
